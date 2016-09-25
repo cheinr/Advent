ed90fca1964a34ce73cadce69ac0d949cd0e8517
dszopa <dszopa@iastate.edu>
2016-09-25 18:33:51 -0500

    Add User Entity

---
+++ advent-backend/src/main/java/com/advent/entity/User.java
@@ -0,0 +1,88 @@
+package com.advent.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.io.File;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    private Long id;
+    private String username;
+    private String displayName;
+    private String email;
+    private String password;
+    private String description;
+    private File profilePicture;
+    // TODO dszopa 9/25/16 - Add List of groups the user is in (groups need to be made first)
+    // TODO dszopa 9/25/16 - Add List of chats the user is in (chats need to be made first)
+
+    @Id
+    @Column(name = "id", unique = true, nullable = false, length = 20)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "generator")
+    @SequenceGenerator(name = "generator", sequenceName = "user_seq", allocationSize = 1)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(name = "username", nullable = false)
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Column(name = "display_name", nullable = false)
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    @Column(name = "email", nullable = false)
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @JsonIgnore
+    @Column(name = "password", nullable = false)
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Column(name = "description")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @JsonIgnore
+    public File getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(File profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+}
