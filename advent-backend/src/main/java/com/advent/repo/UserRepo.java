2263103d8544ea31bb6850177742e85f437974c4
dszopa <dszopa@iastate.edu>
2016-09-25 18:38:32 -0500

    Add UserRepo, Remove ExampleEntity & Repo

--- advent-backend/src/main/java/com/advent/repo/UserRepo.java
+++ advent-backend/src/main/java/com/advent/repo/UserRepo.java
@@ -1,9 +1,10 @@
 package com.advent.repo;
 
-import com.advent.entity.ExampleEntity;
+import com.advent.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ExampleEntityRepo extends JpaRepository<ExampleEntity, Long> {
+public interface UserRepo extends JpaRepository<User, Long> {
+
 }
