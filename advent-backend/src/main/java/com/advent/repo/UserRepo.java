441099f0eb77ea48a550b56323d30617e83ad9fb
dszopa <dszopa@iastate.edu>
2016-09-25 19:05:34 -0500

    Add findByUsername and findByEmail to UserRepo

--- advent-backend/src/main/java/com/advent/repo/UserRepo.java
+++ advent-backend/src/main/java/com/advent/repo/UserRepo.java
@@ -2,9 +2,17 @@ package com.advent.repo;
 
 import com.advent.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepo extends JpaRepository<User, Long> {
+
+    @Query("select u from User u where u.username = :username")
+    User findByUsername(@Param("username") String username);
+
+    @Query("select u from User u where u.email = :email")
+    User findByEmail(@Param("email") String email);
 
 }
