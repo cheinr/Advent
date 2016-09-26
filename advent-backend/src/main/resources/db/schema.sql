820ad1f0fd2f0f530526835c8ba4f8b94ce0e542
dszopa <dszopa@iastate.edu>
2016-09-25 23:09:01 -0500

    Update schema.sql to contain a sample user

--- advent-backend/src/main/resources/db/schema.sql
+++ advent-backend/src/main/resources/db/schema.sql
@@ -1,2 +1,4 @@
-CREATE TABLE example_entity (id bigint(20) NOT NULL AUTO_INCREMENT, characteristic varchar(255) NOT NULL);
-INSERT INTO example_entity (id, characteristic) VALUES (1000, 'This is a sample entity');
+CREATE TABLE user (id bigint(20) NOT NULL AUTO_INCREMENT, username varchar(255) NOT NULL, display_name varchar(255) NOT NULL,
+                    email varchar(255) NOT NULL, password varchar(255) NOT NULL, description varchar(255) NOT NULL);
+INSERT INTO user (id, username, display_name, email, password, description) VALUES (1000, 'exampleUser', 'displayName',
+                    'email@address.com', 'password', 'description');
