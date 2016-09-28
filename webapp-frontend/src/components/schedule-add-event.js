d5b3a87eeee465033a881c9ee6a9b7d91cbb0f31
Colin Heinrichs <chein@iastate.edu>
2016-09-28 13:01:00 -0500

    addded a simple component that contains skeleton for the add-schedule-event view

---
+++ webapp-frontend/src/components/schedule-add-event.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+var ScheduleAddEvent = React.createClass({
+  render: function() {
+    //TODO -find alternative to these <br/> tags
+    return (
+      <div>
+        <h1>Add Event</h1>
+
+        <input type="text" placeholder="name"/>
+        <br/>
+        <br/>
+        <input type="text" placeholder="mm/dd/yy"/>
+        <br/>
+        <br/>
+        <input type="text" placeholder="when"/>
+        <b> to </b>
+        <input type="text" placeholder="when" />
+        <br/>
+        <br/>
+        <b>@ </b>
+        <input type="text" placeholder="location" />
+        <br/>
+        <br/>
+        <button>submit</button>
+      </div>
+    )
+  }
+
+});
+
+export default ScheduleAddEvent;
--- webapp-frontend/src/index.js
+++ webapp-frontend/src/index.js
@@ -6,11 +6,14 @@ import MainLayout from './components/main-layout';
 import App from './components/app';
 import Home from './components/home';
 
+import ScheduleAddEvent from './components/schedule-add-event';
+
 ReactDom.render(
     <Router history={browserHistory}>
         <Route component={MainLayout}>
             <Route path="/" component={App}/>
             <Route path="/main" component={Home} />
+            <Route path="/schedule/addevent" component={ScheduleAddEvent}/>
         </Route>
     </Router>
 , document.querySelector('.container'));
