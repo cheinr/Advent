034b73b7423580b220c49a06b1509bfad6137e7f
dszopa <dszopa@iastate.edu>
2016-10-04 11:00:09 -0500

    Add ESLint support & Clean up Javascript with current standards

---
+++ webapp-frontend/src/components/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import MainFeed from './main-feed';
+
+export default function Home(props) {
+  return (
+    <div className="Home">
+      Welcome User!
+      <MainFeed />
+    </div>
+  );
+}
---
+++ webapp-frontend/src/components/main-feed.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function MainFeed(props) {
+  return (
+    <div className="feed">
+      This is where the feed goes
+    </div>
+  );
+}
