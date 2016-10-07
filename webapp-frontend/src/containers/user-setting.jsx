40d6b5cec93a2ead397e8a19460f54137112f68f
dszopa <dszopa@iastate.edu>
2016-10-07 16:33:10 -0500

    Fix styling for the thumbnail & Add pull-down class

--- webapp-frontend/src/containers/user-setting.jsx
+++ webapp-frontend/src/containers/user-setting.jsx
@@ -14,7 +14,6 @@ export default class UserSetting extends React.Component {
   }
   componentWillMount() {
     axios.get(`/api/users/id/${this.props.params.userId}`)
-    // axios.get('/api/users/id/1')
       .then((response) => {
         console.log(`response data: ${response.data}`);
         this.setState({
@@ -29,15 +28,17 @@ export default class UserSetting extends React.Component {
   render() {
     return (
       <div>
-        <div className="row">
+        <div className="row pull-down">
           <div className="col-xs-3">
-            <a href="#" className="thumbnail">
+            <a href="#" className="thumbnail no-margin-bottom">
               {/* A simple placeholder until we host our own */}
               <img src="http://xacatolicos.com/app/images/avatar/icon-user.png" alt="..." />
             </a>
           </div>
+          <div className="col-xs-6">
+            <EntryField fieldName="Display Name" placeholder={this.state.username} />
+          </div>
         </div>
-        <EntryField fieldName="Display Name" placeholder={this.state.username} />
         <div>
           <TextBox fieldName="Description" placeholder={this.state.description} />
         </div>
