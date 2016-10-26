dfb9ff5065040133b25b1b41deba144088c0330f
Colin Heinrichs <chein@iastate.edu>
2016-10-25 19:16:13 -0500

    Finished Setting up data flow between components

--- webapp-frontend/src/components/chats/chat-message-sender.jsx
+++ webapp-frontend/src/components/chats/chat-message-sender.jsx
@@ -3,12 +3,16 @@ import React from 'react';
 
 export default class ChatMessageSender extends React.Component {
 
+  handleSend() {
+    this.props.handleMessageSend(this.refs.messageInput.value);
+  }
+
   render() {
     return (
       <div>
          <form className="form-inline">
-           <input type="text" className="form-control" />
-           <button type="button" className="btn btn-default">Send</button>
+           <input type="text" ref="messageInput" className="form-control" />
+           <button type="button" className="btn btn-default" onClick={this.handleSend}>Send</button>
          </form>
       </div>
     );
--- webapp-frontend/src/components/chats/group-chat.jsx
+++ webapp-frontend/src/components/chats/group-chat.jsx
@@ -4,41 +4,43 @@ import io from 'socket.io-client';
 import ChatMessageContainer from './chat-message-container';
 import ChatMessageSender from './chat-message-sender';
 
+import auth from "../../auth";
+
+import axios from 'axios';
 
 /*
-*   State variables : messages, newMsgText?
+*   State variables : messages
 *   Prop variables : groupName, groupID - might just pass in group
 */
-const GroupChat = React.createClass({ //withRouter gives this component the router in props
-  getInitialState: function() {
-      return {messages: ["test: one message", "test: two messages"]};
-  },
+export default class GroupChat extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {messages: [
+    {senderName: "Colin", message: "Hello Chat!", date: "10/23/95", id: 0 },
+    {senderName: "Ryan", message: "This is a message", date: "10/23/95", id: 1}]};
+  }
 
-  componentDidMount: function() {
+  componentDidMount() {
+
 
     socket = io();
     socket.on('chat message', function(msg) {
         this.state.messages.push(msg);
     });
-  },
+  }
 
-  sendMessage: function() {
-      socket.emit('chat message', )
-  },
+  handleMessageSend(message) {
+    socket.emit('chat message', message);
+  }
 
-  render: function() {
-    var MESSAGES = [
-    {senderName: "Colin", message: "Hello Chat!", date: "10/23/95", id: 0 },
-    {senderName: "Ryan", message: "This is a message", date: "10/23/95", id: 1}];
+  render() {
 
       return (
         <div>
           <h3>{this.props.groupName} Chat</h3>
-          <ChatMessageContainer messages={MESSAGES} />
-          <ChatMessageSender />
+          <ChatMessageContainer messages={this.state.messages} />
+          <ChatMessageSender onSend={this.handleMessageSend} />
         </div>
       );
     }
-});
-
-export default GroupChat;
+}
