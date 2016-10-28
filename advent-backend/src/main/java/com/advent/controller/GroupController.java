5934da7baf36e45eb52f67b3237b356dbc0e04dc
sglancy <sglancy@iastate.edu>
2016-10-28 14:29:02 -0500

    Progress check on GroupController

--- advent-backend/src/main/java/com/advent/controller/GroupController.java
+++ advent-backend/src/main/java/com/advent/controller/GroupController.java
@@ -4,6 +4,7 @@ import com.advent.entity.Group;
 import com.advent.service.GroupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,4 +18,24 @@ public class GroupController {
     public Group getGroupInfo() {
         return null;
     }
+
+    //creates new Group
+    @RequestMapping(value = "/group/new", method = RequestMethod.POST)
+    public GroupDTO newGroup(HttpServletRequest request) {return GroupService.newGroup(request);}
+
+    @RequestMapping(value = "/group/edit", method = RequestMethod.POST)
+    public GroupDTO editGroup(@RequestBody GroupDTO groupDTO) {
+        return GroupService.editGroup(groupDTO);
+    }
+
+    @RequestMapping(value = "/group/members/{groupId}", method = RequestMethod.GET)
+    public GroupDTO getMembers(@PathVariable Long groupId) {
+        return GroupService.findMembers(groupId);
+    }
+
+    @RequestMapping(value = "/group/tags", method = RequestMethod.POST)
+    public GroupDTO addTag(@RequestBody GroupDTO groupDTO) {
+        return GroupService.addTag(groupDTO);
+    }
+
 }
