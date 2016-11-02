86d9c761fae50e3dc0f29a52faa330f9d2fffd53
dszopa <dszopa@iastate.edu>
2016-11-01 23:17:31 -0500

    Add AnnouncementController

---
+++ advent-backend/src/main/java/com/advent/controller/AnnouncementController.java
@@ -0,0 +1,36 @@
+package com.advent.controller;
+
+import com.advent.dto.AnnouncementDTO;
+import com.advent.service.AnnouncementService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class AnnouncementController {
+
+    @Autowired
+    private AnnouncementService announcementService;
+
+    @RequestMapping(value = "/announcement/create", method = RequestMethod.POST)
+    public AnnouncementDTO createAnnouncement(@RequestBody AnnouncementDTO announcementDTO) {
+        return announcementService.createAnnouncement(announcementDTO);
+    }
+
+    @RequestMapping(value = "/announcement/delete/id/{announcementId}", method = RequestMethod.POST)
+    public void deleteAnnouncement(@PathVariable Long announcementId) {
+        announcementService.deleteAnnouncementById(announcementId);
+    }
+
+    @RequestMapping(value = "/announcement/homepage/user/{userId}", method = RequestMethod.GET)
+    public List<AnnouncementDTO> getHomepageAnnouncementsForUser(@PathVariable Long userId) {
+        return announcementService.getHomepageAnnouncementsForUser(userId);
+    }
+
+    @RequestMapping(value = "/announcement/group/{groupId}", method = RequestMethod.GET)
+    public List<AnnouncementDTO> getAllAnnouncementsForGroup(@PathVariable Long groupId) {
+        return announcementService.getAllAnnouncementsForGroup(groupId);
+    }
+}
