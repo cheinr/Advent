490428841f618c6e95e750e4a7f8059a25acbf7e
Colin Heinrichs <chein@iastate.edu>
2016-11-04 10:57:22 -0500

    Fixed issues; Set up chat page to have loading icon.

--- webapp-frontend/src/containers/GroupChatContainer.jsx
+++ webapp-frontend/src/containers/GroupChatContainer.jsx
@@ -1,77 +1,83 @@
-import React, {Component} from 'react';
+import React from 'react';
 import io from 'socket.io-client';
 import axios from 'axios';
-import withRouter from 'react-router';
+import {withRouter} from 'react-router';
 
 import ChatMessageContainer from '../components/chats/chat-message-container';
 import ChatMessageSender from '../components/chats/chat-message-sender';
 
 /*
 *  Container meant to be the handler for /chat/group/:groupId
-*  You must give it access to the router in props.
+*  You must give it access to the router in props. (Trying to change this)
 *  (Call withRouter(GroupChatContainer))
 */
 
-export default class GroupChatContainer extends React.Component {
-    constructor(props) {
-	super(props);
-	this.state = {messages: [] };
+const GroupChatContainer = withRouter(React.createClass( {
+    getInitialState() {
+	return { messages: [], groupName: "", loading: true };
+    },
+
+    componentDidMount : function() {
 	this.socket = null;
-	this.connect = this.connect.bind(this);
 
-    }
-
-    componentDidMount() {
-	
 	var url = `/api/group/${this.props.params.groupId}`;
 	console.log("url: " + url);
 	//check if group exists and get the name of the group.
 	axios.get(url).then( (resp) => {
 	    console.log(resp);
 	    if(resp.data != "") {
-		this.setState( {groupName: resp.data.groupName});
-v		this.socket = this.connect(this.props.params.groupId);
-		this.socket.on('chat message', function(msg) {
+		this.setState({groupName: resp.data.groupName, loading: false});
+		this.socket = this.connect(this.props.params.groupId);
+		this.socket.on('chat message', (msg) => {
 		    var messages = this.state.messages;
 		    messages.push(msg);
 		    console.log(messages);
 		    this.setState({messages: messages} );
-		}.bind(this));
+		});
 		this.render();
 	    } else {
 		//redirect user to main page if this one doesn't exist.
+		//TODO - redirect to previous page?
 		this.props.router.replace("/");
 		console.log("group doesn't exist");
 	    }
 	}).catch( function(error) {
 	    console.log(error);
 	});
+    },
 
-    }
-
-    handleMessageSend(message) {
+    handleMessageSend: function(message) {
 	//data to send
 	//TODO - get actual user name
-	var data = {senderName: "Colin", message: message,
+	var data = {senderName: this.props.user.displayName, message: message,
 		    groupId: this.props.params.groupId};
         this.socket.emit('chat message', data);
-    }
+    },
 
-    connect(groupId) {
+    connect: function(groupId) {
         return io().emit("join-room", groupId);
+    },
+
+    render: function() {
+	if(this.state.loading) {
+	    return (
+		<div>
+		    <i className="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
+		    <span className="sr-only">Loading...</span>
+		</div>
+	    );
+	} else {
+            return (
+		<div>
+		    <h3>{this.state.groupName} Chat</h3>
+		    <ChatMessageContainer messages={this.state.messages} />
+		    <ChatMessageSender onSend={this.handleMessageSend} />
+		</div>
+            );
+	}
     }
+}));
 
-    render() {
-
-        return (
-            <div>
-		<h3>{this.state.groupName} Chat</h3>
-		<ChatMessageContainer messages={this.state.messages} />
-		<ChatMessageSender onSend={this.handleMessageSend.bind(this)} />
-            </div>
-        );
-    }
-}
-
+export default GroupChatContainer;
 
 
