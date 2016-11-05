3e36d14e263d88f43163930f987b1de609b383dd
Colin Heinrichs <chein@iastate.edu>
2016-11-05 18:31:34 -0500

    Added edit button to viewUserInfo page when viewing currently logged in user. Changed the 'My Profile' link in the toolbar dropdown to link to user details page.

--- webapp-frontend/src/containers/ViewUserContainer.jsx
+++ webapp-frontend/src/containers/ViewUserContainer.jsx
@@ -4,55 +4,70 @@ import ReactMarkdown from 'react-markdown';
 import Panel from '../components/display/Panel';
 import Thumbnail from '../components/display/Thumbnail';
 import PageHeader from '../components/display/PageHeader';
+import {Link} from 'react-router';
 
 export default class ViewUserContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      displayName: '',
-      description: '',
-      pictureUrl: '',
-    };
-  }
+    constructor(props) {
+	super(props);
+	this.state = {
+	    displayName: '',
+	    description: '',
+	    pictureUrl: '',
+	};
+    }
+    
+    componentWillMount() {	
+	const url = `/api/users/id/${this.props.params.userId}`;
+	axios.get(url)
+	     .then((response) => {
+		 this.setState({
+		     displayName: response.data.displayName,
+		     description: response.data.description,
+		     pictureUrl: response.data.pictureUrl,
+		 });
+	     })
+	     .catch((error) => {
+		 console.log(error);
+		 this.setState({
+		     showErrors: true,
+		     errorMessage: 'There was an error receiving the user from the server',
+		 });
+	     });
+    }
 
-  componentWillMount() {
-    const url = `/api/users/id/${this.props.params.userId}`;
-    axios.get(url)
-      .then((response) => {
-        this.setState({
-          displayName: response.data.displayName,
-          description: response.data.description,
-          pictureUrl: response.data.pictureUrl,
-        });
-      })
-      .catch((error) => {
-        console.log(error);
-        this.setState({
-          showErrors: true,
-          errorMessage: 'There was an error receiving the user from the server',
-        });
-      });
-  }
+    render() {
+	var edit_button = "";
+	if(this.props.user != null
+	   && this.props.user.id == this.props.params.userId) {
+	    edit_button = (
+		<Link to={`/user/edit/${this.props.params.userId}`}>
+		    <button type="button" className="btn btn-default">
+			Edit</button>
+		</Link>
+	    );	
+	} else {
+	    edit_button = "";
+	}
 
-  render() {
-    return (
-      <div>
-        {this.state.showErrors ? <Error>{this.state.errorMessage}</Error> : null}
-        <div className="row pull-down">
-          <div className="col-xs-3">
-            <Thumbnail pictureUrl={this.state.pictureUrl} altText="User" />
-          </div>
-          <div className="col-xs-9">
-            <PageHeader title={this.state.displayName} />
-          </div>
-        </div>
-        <div className="row">
-          <Panel title="Description">
-            <ReactMarkdown escapeHtml source={this.state.description} />
-          </Panel>
-        </div>
-        {/* TODO dszopa 10/18/16 - Display groups too */}
-      </div>
-    );
-  }
+	return (
+	    <div>
+		{this.state.showErrors ? <Error>{this.state.errorMessage}</Error> : null}
+		<div className="row pull-down">
+		    <div className="col-xs-3">
+			<Thumbnail pictureUrl={this.state.pictureUrl} altText="User" />
+		    </div>
+		    <div className="col-xs-9">
+			<PageHeader title={this.state.displayName} />
+		    </div>
+		</div>
+		<div className="row">
+		    <Panel title="Description">
+			<ReactMarkdown escapeHtml source={this.state.description} />
+		    </Panel>
+		</div>
+		{edit_button}
+		{/* TODO dszopa 10/18/16 - Display groups too */}
+	    </div>
+	);
+    }
 }
