251340a3ca329c827a110452602157300f76228b
Colin Heinrichs <chein@iastate.edu>
2016-11-28 14:22:02 -0600

    Finished LocationMap component.

--- webapp-frontend/src/components/LocationMap.jsx
+++ webapp-frontend/src/components/LocationMap.jsx
@@ -3,17 +3,22 @@ import React, { Component } from 'react';
 export default class LocationMap extends Component {
 
     componentDidMount() {
+	var loc = {lat: this.props.lat, lng: this.props.lng};
         var map = new google.maps.Map(document.getElementById('areaMap'), {
             center: {lat: this.props.lat, lng: this.props.lng},
-            zoom: 8
+            zoom: 15
         });
+	var marker = new google.maps.Marker({
+	    position: loc,
+	    map: map
+	});
     }
 
     render() {
         return (
-            <div>
+            <div className="padded-bottom">
                 <h3>Area Map</h3>
-                <div id="areaMap">Loading...</div>
+		<div id="areaMap">Loading...</div>
             </div>
         );
     }
