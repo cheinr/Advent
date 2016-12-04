ec5d7c917874c33a64c2e94ae782323c07952ef4
dszopa <dszopa@iastate.edu>
2016-12-03 19:19:27 -0600

    Update Schema event data & EventCalendar, Add functionality in EventCalendarContainer & index.html to export an event to google calendar

--- webapp-frontend/src/components/EventCalendar.jsx
+++ webapp-frontend/src/components/EventCalendar.jsx
@@ -4,60 +4,29 @@ import moment from 'moment';
 import { Link } from 'react-router';
 
 BigCalendar.setLocalizer(
-    BigCalendar.momentLocalizer(moment)
+  BigCalendar.momentLocalizer(moment)
 );
 
-function Event({ event }) {
-    return (
-        <span>
-      <strong>
-      {event.title}
-      </strong>
-            { event.desc && (':  ' + event.desc)}
-    </span>
-    )
-}
-//
-// function EventAgenda({ event }) {
-//     // todo fix this probably will not work as this is not defined in this scope
-//     return (
-//         <span>
-//             <em>{event.title}</em>
-//             <p>{ event.desc }</p>
-//             <Link to={`/event/${event.id}`}>View Event</Link>
-//
-//             <a onClick={() => this.props.exportToGCal(event)}>View Event</a>
-//         </span>
-//     )
-// }
-
 export default class EventCalendar extends Component {
-    componentDidMount() {
-
-    }
-
-    render() {
-        console.log(this.props.Event);
-        console.log(Event);
-        return (
-            <div>
-                <Link to={`/group/${this.props.groupId}`}>Back to Group</Link>
-                <div>
-                    <BigCalendar
-                        {...this.props}
-                        events={this.props.events}
-                        timeslots={2}
-                        defaultDate={new Date()}
-                        defaultView="agenda"
-                        components={{
-                            event: this.props.Event,
-                            agenda: {
-                                event: this.props.EventAgenda
-                            }
-                        }}
-                    />
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Link to={`/group/${this.props.groupId}`}>Back to Group</Link>
+        <div>
+          <BigCalendar
+            events={this.props.events}
+            timeslots={2}
+            defaultDate={new Date()}
+            defaultView="agenda"
+            components={{
+              event: this.props.Event,
+              agenda: {
+                event: this.props.EventAgenda,
+              },
+            }}
+          />
+        </div>
+      </div>
+    );
+  }
 }
--- webapp-frontend/src/containers/EventCalendarContainer.jsx
+++ webapp-frontend/src/containers/EventCalendarContainer.jsx
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import moment from 'moment';
-import EventCalendar from '../components/EventCalendar';
 import { Link } from 'react-router';
+import EventCalendar from '../components/EventCalendar';
+import Alert from '../components/feedback/Alert'
 
 export default class EventCalendarContainer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            events: [],
-        };
-        this.exportToGCal = this.exportToGCal.bind(this);
-        this.Event = this.Event.bind(this);
-        this.EventAgenda = this.EventAgenda.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: [],
+      showAddedEvent: false,
+      eventAddMessage: '',
+      eventAddLink: '',
+    };
+    this.exportToGCal = this.exportToGCal.bind(this);
+    this.event = this.event.bind(this);
+    this.eventAgenda = this.eventAgenda.bind(this);
+  }
 
-  componentDidMount() {
+  componentWillMount() {
     const url = `/api/event/group/${this.props.params.groupId}`;
 
     axios.post(url)
@@ -27,8 +31,9 @@ export default class EventCalendarContainer extends Component {
             id: event.id,
             title: event.name,
             desc: event.description,
-            start: moment(event.startDate, 'YYYY-MM-DD HH:mm:ss').toDate(),
-            end: moment(event.endDate, 'YYYY-MM-DD HH:mm:ss').toDate(),
+            start: moment(event.startDate, 'MM/DD/YYYY HH:mm TT').toDate(),
+            end: moment(event.endDate, 'MM/DD/YYYY HH:mm TT').toDate(),
+            location: event.location,
           });
         });
         this.setState({ events: eventArr });
@@ -38,45 +43,88 @@ export default class EventCalendarContainer extends Component {
       });
   }
 
-    exportToGCal(event) {
-        // TOOD post to gcal to create event in google calendars
-        console.log("hit");
-    }
+  exportToGCal(event) {
+    gapi.client.load('calendar', 'v3', () => {
+      /* eslint-disable */
+      const gCalEvent = {
+        'summary': event.title,
+        'location': event.location,
+        'description': event.desc,
+        'start': {
+          'dateTime': event.start.toISOString(),
+          'timeZone': 'America/Chicago'
+        },
+        'end': {
+          'dateTime': event.end.toISOString(),
+          'timeZone': 'America/Chicago'
+        },
+        'reminders': {
+          'useDefault': false,
+          'overrides': [
+            {'method': 'email', 'minutes': 24 * 60},
+            {'method': 'popup', 'minutes': 10}
+          ]
+        }
+      };
+      /* eslint-enable */
 
-    Event(event) {
-        return (
-            <span>
-                <strong>
-                    {event.title}
-                </strong>
-                {event.desc && (':  ' + event.desc)}
-            </span>
-        )
-    }
+      const request = gapi.client.calendar.events.insert({
+        calendarId: 'primary',
+        resource: gCalEvent,
+      });
 
-    EventAgenda(event) {
-        // todo fix this probably will not work as this is not defined in this scope
-        return (
-            <span>
-                <em>{event.title}</em>
-                <p>{ event.desc }</p>
-                <Link to={`/event/${event.id}`}>View Event</Link>
-                <a onClick={() => this.exportToGCal(event)}>Export to Google Calendar</a>
-            </span>
-        )
-    }
+      request.execute((googleEvent) => {
+        this.setState({
+          showAddedEvent: true,
+          eventAddMessage: `${event.title} has been added to your google calendar!`,
+          eventAddLink: googleEvent.htmlLink,
+        });
+      });
+    });
+  }
 
-    render() {
-        return (
-            <div className="calendar-container">
-                <EventCalendar
-                    events={this.state.events}
-                    groupId={this.props.params.groupId}
-                    exportToGCal={(event) => {this.exportToGCal(event)}}
-                    Event={({event}) => {return this.Event(event)}}
-                    EventAgenda={({event}) => {return this.EventAgenda(event)}}
-                />
-            </div>
-        )
-    }
+  event(event) {
+    return (
+      <span>
+        <strong>
+          {event.title}
+        </strong>
+        {event.desc && (':  ' + event.desc)}
+      </span>
+    );
+  }
+
+  eventAgenda(event) {
+    return (
+      <span>
+        <em>{event.title}</em>
+        <p>{ event.desc }</p>
+        <div className="btn-group">
+          <Link className="btn btn-default" to={`/event/${event.id}`}>View Event</Link>
+          <a className="btn btn-default" onClick={() => this.exportToGCal(event)}>Export to Google Calendar</a>
+        </div>
+      </span>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.showAddedEvent ? (
+          <Alert>
+            <p>{this.state.eventAddMessage} You can view it <a href={this.state.eventAddLink}>here.</a></p>
+          </Alert>
+        ) : null}
+        <div className="calendar-container">
+          <EventCalendar
+            events={this.state.events}
+            groupId={this.props.params.groupId}
+            exportToGCal={({ event }) => this.exportToGCal(event)}
+            Event={({ event }) => this.event(event)}
+            EventAgenda={({ event }) => this.eventAgenda(event)}
+          />
+        </div>
+      </div>
+    );
+  }
 }
